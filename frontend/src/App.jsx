a1d60ecee1db58bceff2a18026363796cb867f6e
Kamal <kamalacca@gmail.com>
2024-03-02 04:57:10 +0400

    add daisy ui package and basic page structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
-
+import Login from "./pages/login/Login";
 
 function App() {
-
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <div className="p-4 h-screen flex items-center justify-center">
+        <Login />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
