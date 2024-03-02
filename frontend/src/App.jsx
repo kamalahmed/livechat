72f1ddb6884014cb14074fbb55d577606ea1b739
Kamal <kamalacca@gmail.com>
2024-03-02 05:24:07 +0400

    add sign up component ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/Signup";
 
 function App() {
   return (
     <>
       <div className="p-4 h-screen flex items-center justify-center">
-        <Login />
+        <SignUp />
       </div>
     </>
   );
